a1617711e96d46e1b019166f569b35bc8d7cf2ea
Caleb Appiagyei <caleba04@vt.edu>
2025-01-02 21:48:07 -0500

    installing dotenv

--- server/database.js
+++ server/database.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const {Pool} = require("pg")
 
 const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    database: process.env.DB
 })
+
+pool.query("SELECT * FROM information_schema.tables WHERE table_schema = 'coach_schema';").then((res) => {
+    console.log("Grabbing all tables within schema");
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+});
+
+module.exports = pool;
